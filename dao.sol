b60cecc01249d0fef9d96ae174381bd5fed40cae
Tanya Agarwal <74188740+t-cpu494@users.noreply.github.com>
2023-09-15 02:06:44 +0530

    Update dao.sol

--- dao.sol
+++ dao.sol
@@ -6,24 +6,33 @@ contract DAO {
         string description;
         uint amount;
         uint totalShares;
+        uint priceOf1share;
         address payable recipient;
         uint votes;
         bool isExecuted;
     }
 
-    mapping(address => mapping(address => bool)) private isInvestor;
+    struct isInvestor {
+        address investor;
+        bool isInvestorOrNot;
+    }
+
+    mapping(address => mapping(address => bool)) private isInvestorInTheProposal;
     mapping(address => bool) public isProposer;
     mapping(address => mapping(address => uint)) private numOfSharesOfAnInvestorInAProposal;
     mapping(address => mapping(address => bool)) private whoVotedForWhichProposal;
-    mapping(address => Proposal) public proposals;
+    Proposal[] public proposals;
+    isInvestor[] private investorArray;
 
     uint public votingStartTime;
     uint public votingDuration;
     uint public votingEndTime = votingStartTime + votingDuration;
     uint public quorum;
     address public manager;
+    address public wp;
 
     constructor(uint _votingStartTime, uint _votingDuration, uint _quorum) {
+        require(_votingStartTime > block.timestamp, "Not a valid voting start time!");
         require(_quorum > 50 && _quorum < 100, "Not a valid quorum value!");
         votingStartTime = _votingStartTime;
         votingDuration = _votingDuration;
@@ -39,53 +48,122 @@ contract DAO {
     function redeemShares(uint numOfShares, address proposalId) external {
         require(numOfSharesOfAnInvestorInAProposal[msg.sender][proposalId] >= numOfShares, "You don't have enough shares invested in this proposal!");
         numOfSharesOfAnInvestorInAProposal[msg.sender][proposalId] -= numOfShares;
-        payable(msg.sender).transfer((proposals[proposalId].amount/proposals[proposalId].totalShares)*numOfShares);
-        if(numOfSharesOfAnInvestorInAProposal[msg.sender][proposalId] == 0) {
-            isInvestor[msg.sender][proposalId] == false;
+        for(uint i = 0; i < proposals.length; i++) {
+            if(proposals[i].recipient == proposalId) {
+        payable(msg.sender).transfer((proposals[i].priceOf1share)*numOfShares);
+            }
         }
-    }
+        if(numOfSharesOfAnInvestorInAProposal[msg.sender][proposalId] == 0) {
+            isInvestorInTheProposal[msg.sender][proposalId] = false;
+        }
+        for(uint i = 0; i <= proposals.length; i++) {
+            if(i == proposals.length) {
+                for(uint j = 0; j < investorArray.length; j++) {
+                    if(investorArray[j].investor == msg.sender) {
+                        investorArray[j].isInvestorOrNot = false;
+                    }
+                }
+            }
+            if(isInvestorInTheProposal[msg.sender][proposals[i].recipient] == true) {
+                break;
+            }
+            }
+        }
 
     function transferShare(uint numOfShares, address to, address proposalId) external {
         require(numOfSharesOfAnInvestorInAProposal[msg.sender][proposalId] >= numOfShares, "You don't have enough shares invested in this proposal");
         numOfSharesOfAnInvestorInAProposal[msg.sender][proposalId] -= numOfShares;
         numOfSharesOfAnInvestorInAProposal[to][proposalId] += numOfShares;
-        isInvestor[to][proposalId] = true;
-        if(numOfSharesOfAnInvestorInAProposal[msg.sender][proposalId] == 0) {
-            isInvestor[msg.sender][proposalId] = false;
+        isInvestorInTheProposal[to][proposalId] = true;
+        for(uint i = 0; i <= investorArray.length; i++) {
+            if(i == investorArray.length) {
+                investorArray.push(isInvestor(to, true));
+            }
+            if(investorArray[i].investor == to) {
+                investorArray[i].isInvestorOrNot = true;
+            }
         }
+        if(numOfSharesOfAnInvestorInAProposal[msg.sender][proposalId] == 0) {
+            isInvestorInTheProposal[msg.sender][proposalId] = false;
+        }
+        for(uint i = 0; i <= proposals.length; i++) {
+            if(i == proposals.length) {
+                for(uint j = 0; j < investorArray.length; j++) {
+                    if(investorArray[j].investor == msg.sender) {
+                        investorArray[j].isInvestorOrNot = false;
+                    }
+                }
+            }
+            if(isInvestorInTheProposal[msg.sender][proposals[i].recipient] == true) {
+                break;
+            }
+            }        
     }
 
-    function createProposal(string calldata description, uint amount, uint totalShares, address payable recipient) public onlyManager() {
-        proposals[recipient] = Proposal(description, amount, totalShares, recipient, 0, false);
+    function createProposal(string calldata _description, uint _amount, uint _totalShares, address payable _recipient) external onlyManager() {
+        require(block.timestamp < votingStartTime, "Proposal creation time ended!");
+        proposals.push(Proposal(_description, _amount, _totalShares, _amount/_totalShares, _recipient, 0, false));
+        isProposer[_recipient] = true;
     }
 
     function voteProposal(address proposalId) external {
         require(block.timestamp > votingStartTime, "Voting not started yet!");
         require(block.timestamp < votingEndTime, "Voting Ended!");
-        Proposal storage proposal = proposals[proposalId];
+        for(uint i = 0; i < proposals.length; i++) {
+            if(proposals[i].recipient == proposalId) {
+                require(proposals[i].isExecuted == false, "It is already executed!");
+            }}
         require(whoVotedForWhichProposal[msg.sender][proposalId] == false, "You have already voted for this proposal!");
-        require(proposal.isExecuted == false, "It is already executed!");
         whoVotedForWhichProposal[msg.sender][proposalId] = true;
-        proposal.votes += numOfShares[msg.sender];
-    }
-
-    function executeProposal(uint proposalId) public onlyManager() {
-        Proposal storage proposal = proposals[proposalId];
-        require(((proposal.votes*100)/totalShares) >= quorum, "Majority does not support");
-        proposal.isExecuted = true;
-        availableFunds -= proposal.amount;
-        _transfer(proposal.amount, proposal.recipient);
-    }
-
-    function _transfer(uint amount, address payable recipient) private {
-        recipient.transfer(amount);
-    }
-
-    function ProposalList() public view returns(Proposal[] memory) {
-        Proposal[] memory arr = new Proposal[](nextProposalId - 1);
-        for(uint i = 0; i < nextProposalId; i++) {
-            arr[i] = proposals[i];
+        for(uint i = 0; i <= investorArray.length; i++) {
+            if(i == investorArray.length) {
+                investorArray.push(isInvestor(msg.sender, false));
+            }
+            if(investorArray[i].investor == msg.sender) {
+                investorArray[i].isInvestorOrNot = false;
+            }
         }
-        return arr;
+    }
+
+    function winnerProposal() external onlyManager() {
+        require(block.timestamp > votingStartTime, "Voting has not ended yet!");
+        uint majorityVotes;
+        uint t;
+        for(uint i = 0; i < proposals.length; i++) {
+            for(uint j = 0; j < investorArray.length; j++) {
+                if(whoVotedForWhichProposal[investorArray[j].investor][proposals[i].recipient] == true ) {
+                    t++;
+                }
+            }
+            if(t >= majorityVotes) {
+                majorityVotes = t;
+                wp = proposals[i].recipient;
+            }
+        }
+        for(uint i = 0; i < proposals.length; i++) {
+            if(proposals[i].recipient == wp) {
+                proposals[i].isExecuted = true;
+            }
+        }
+    }
+
+    function purchaseShares(uint numOfShares) external {
+        uint i;
+        for(uint j = 0; j < proposals.length; j++) {
+            if(proposals[j].recipient == wp) {
+                i = j;
+            }
+        }
+        require(proposals[i].totalShares >= numOfShares, "Not enough shares left now to purchase!");
+        proposals[i].totalShares -= numOfShares;
+        numOfSharesOfAnInvestorInAProposal[msg.sender][wp] = numOfShares;
+        isInvestorInTheProposal[msg.sender][wp] = true;
+        for(uint j = 0; j < investorArray.length; j++) {
+            if(investorArray[j].investor == msg.sender) {
+                investorArray[j].isInvestorOrNot = true;
+            }
+        }
+        payable(wp).transfer((proposals[i].priceOf1share)*numOfShares);
+        proposals[i].amount -= (proposals[i].priceOf1share)*numOfShares;
     }
 }
